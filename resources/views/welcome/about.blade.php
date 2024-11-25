411f789de7891b39359fe96278b41f7621667e47
edescePeru <edesceperu@gmail.com>
2024-11-25 14:41:35 -0500

    about description | Remso

--- resources/views/welcome/about.blade.php
+++ resources/views/welcome/about.blade.php
@@ -23,25 +23,42 @@
 @section('content')
 
     <div class="row pt-5">
-        <div class="col-md-6 ">
-            <div class="img-box">
-                <img src="{{ asset('landing/images/about-img1.png') }}" alt="" class="img-fluid">
-            </div>
-        </div>
         <div class="col-md-6">
             <div class="detail-box">
                 <div class="heading_container">
                     <h2>
-                        We Are Fuego y Masa
+                        Fuego y Masa
                     </h2>
                 </div>
+                <br><br>
                 <p>
-                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-                    in some form, by injected humour, or randomised words which don't look even slightly believable. If you
-                    are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in
-                    the middle of text. All
+                    En Fuego y Masa, somos una pizzería orgullosamente trujillana, apasionada por crear momentos inolvidables a través de sabores únicos.
                 </p>
 
+                <p>
+                    Nuestro <b>objetivo</b> no es solo ofrecerte las pizzas clásicas que amas, 
+                    sino también sorprenderte con experiencias gastronómicas que despierten tus sentidos y redefinan lo que significa disfrutar de una pizza.
+                </p>
+
+                <p>
+                    Sabemos que las mejores historias comienzan en casa, y por eso llevamos nuestros sabores directamente a tu puerta. 
+                    Cada pizza está elaborada con ingredientes cuidadosamente seleccionados y una dedicación que garantiza calidad en cada bocado. 
+                    Queremos ser parte de tus momentos especiales, esos que compartes con familia, amigos o simplemente contigo mismo.
+                </p>
+
+                <p>
+                    Porque en Fuego y Masa, creemos que una pizza es más que comida, es el puente hacia recuerdos inolvidables.
+                </p>
+
+                <p style="text-align: center;" >
+                    <b>"Una pizza, mil recuerdos compartidos."</b>    
+                </p>
+
+            </div>
+        </div>
+        <div class="col-md-6 ">
+            <div class="img-box">
+                <img style="margin: -80px 0px 0px 0px;" src="{{ asset('landing/images/about-img1.png') }}" alt="" class="img-fluid">
             </div>
         </div>
     </div>
