393d14d5f2eacab2d5d91594965350036c570fa0
arcadio1894 <joryes1894@gmail.com>
2024-12-08 20:05:13 -0500

    CAmbio en el checkout js

--- resources/views/product/checkout.blade.php
+++ resources/views/product/checkout.blade.php
@@ -151,7 +151,7 @@
                     <!-- Sección para método de pago en efectivo -->
                     <div id="cash-section" style="display: none; margin-top: 15px;">
                         <label for="cashAmount">Monto con el que paga</label>
-                        <input type="number" class="form-control" id="cashAmount" placeholder="Ingrese monto en efectivo">
+                        <input type="number" class="form-control" id="cashAmount" step="0.01" min="0" placeholder="Ingrese monto en efectivo">
                     </div>
 
                     <!-- Sección para método de pago Yape/Plin -->
